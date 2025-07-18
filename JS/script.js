9c81ede52e45507e93e9bf71cc6e6c7f62b6bc76
GennaroBuonomo <gennarobuonomo66@gmail.com>
2025-07-18 17:34:33 +0200

    fix js

--- JS/script.js
+++ JS/script.js
@@ -1,12 +1,10 @@
 const number = document.getElementById('email-list');
 
-const emails = [];
 
-  for(let i=0; i<10; i++){
-    axios.get(`https://flynn.boolean.careers/exercises/api/random/mail`).then((resp) => {
-
-      console.log(emails);
-
-      number.innerHTML += `<li>${resp.data.response}</li>`
-    })
-  }
+for(let i = 0; i < 10; i++){
+  axios.get(`https://flynn.boolean.careers/exercises/api/random/mail`)
+  .then((resp) => {
+    console.log(resp.data.response);
+    number.innerHTML += `<li>${resp.data.response}</li>`
+  })
+}
