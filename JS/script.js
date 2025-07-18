7dea040a4ac0d6e486602ff32ee2018b0f70b100
GennaroBuonomo <gennarobuonomo66@gmail.com>
2025-07-18 17:04:47 +0200

    fix inserimento axios

--- JS/script.js
+++ JS/script.js
@@ -3,6 +3,13 @@ const number = document.getElementById('email-list');
 const emails = [];
 
   for(let i=0; i<10; i++){
-    console.log(emails);
+    axios.get(`https://flynn.boolean.careers/exercises/api/random/mail`).then((resp) => {
 
+      console.log(emails);
+      
+
+    })
+    
+
+    
   }
