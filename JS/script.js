f10ffd74f3d7327c9ef3a24b2489effca6043a88
GennaroBuonomo <gennarobuonomo66@gmail.com>
2025-07-21 10:07:23 +0200

    fix for

--- JS/script.js
+++ JS/script.js
@@ -1,10 +1,10 @@
-const number = document.getElementById('email-list');
+const emailsList = document.getElementById('email-list');
 
 
 for(let i = 0; i < 10; i++){
   axios.get(`https://flynn.boolean.careers/exercises/api/random/mail`)
   .then((resp) => {
     console.log(resp.data.response);
-    number.innerHTML += `<li>${resp.data.response}</li>`
+    emailsList.innerHTML += `<li>${resp.data.response}</li>`
   })
 }
